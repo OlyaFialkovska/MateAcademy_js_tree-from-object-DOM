0f3624c0d2b35c83eed84c1d9c902e6e8f51e873
Olya <fialkaolya08@gmail.com>
2026-08-16 19:07:28 +0200

    make build

---
+++ MateAcademy_js_tree-from-object-DOM.cc9c0ce3.js
@@ -0,0 +1,2 @@
+"use strict";var food={Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}},tree=document.querySelector("#tree");function createTree(e,r){var t=document.createElement("ul");for(var n in r){var a=document.createElement("li");a.textContent=n,t.append(a),Object.keys(r[n]).length>0&&createTree(a,r[n])}e.append(t)}createTree(tree,food);
+//# sourceMappingURL=MateAcademy_js_tree-from-object-DOM.cc9c0ce3.js.map
